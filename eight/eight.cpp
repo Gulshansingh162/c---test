d031f5317321d6a9a69d97777daccb0cbbf6fcc4
Gulshansingh162 <kumargulshan2968@gmail.com>
2025-08-28 15:39:01 +0530

    test1

---
+++ eight/eight.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,s,rev=0;
+    cout<<"enter the no to be reverse";
+    cin>>a;
+    while(a>0){
+        s=a%10;
+        a=a/10;
+        rev=(rev*10)+s;
+    }
+    cout<<rev;
+    return 0;
+}
